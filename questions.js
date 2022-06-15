81e4cd50fe27a53229ff63ce4c5ac9188467f214
szdygal <sdzygal@gmail.com>
2022-06-14 20:30:22 -0600

    First commit

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Input, aggregate or store operational data",
+        answer: "",
+        options: [
+            "Fully automatically",
+            "With a human intermediary"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Build a dashboard or a report",
+        answer: "",
+        options: [
+            "Online",
+            "Periodically"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Infer (measure indirectly) a process variable, a material property or a key performance indicator",
+        answer: "",
+        options: [
+            "Continuously (e.g. min-by-min)",
+            "Periodically (e.g. once an hour)",
+            "Exactly (quantitatively)",
+            "Approximately (categorically)"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Predict (forecast) a process variable or a key performance indicator",
+        answer: "",
+        options: [
+            "Continuously (e.g. min-by-min)",
+            "Periodically (e.g. once an hour)",
+            "Exactly (quantitatively)",
+            "Approximately (categorically)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Understand causes (influencers) of a process variable or a key performance indicator",
+        answer: "",
+        options: [
+            "Historically (e.g. both 1 week ago and today)",
+            "In real-time (i.e. over the past 10 min)",
+            "Of something noted/recorded",
+            "Of something missed/ not recorded"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Find setpoints to improve (optimize) a process variable or a key performance indicator",
+        answer: "",
+        options: [
+            "On average (probabilistically) over a period of time",
+            "In real time"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Find out how operators act in various situations",
+        answer: "",
+        options: [
+            "In response to specific events",
+            "In general (i.e. infer operating rules)"
+        ]
+    },
+];
