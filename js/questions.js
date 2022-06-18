c078095fb3b308ca3c9e36d2f16d064bd71c7a60
szdygal <sdzygal@gmail.com>
2022-06-18 14:55:45 -0600

    Final commit

--- js/questions.js
+++ js/questions.js
@@ -1,70 +1,56 @@
-// let questions = [{
-//         numb: 1,
-//         question: "Input, aggregate or store operational data",
-//         answer: "",
-//         options: [
-//             "Fully automatically",
-//             "With a human intermediary"
-//         ]
-//     },
-//     {
-//         numb: 2,
-//         question: "Build a dashboard or a report",
-//         answer: "",
-//         options: [
-//             "Online",
-//             "Periodically"
-//         ]
-//     },
-//     {
-//         numb: 3,
-//         question: "Infer (measure indirectly) a process variable, a material property or a key performance indicator",
-//         answer: "",
-//         options: [
-//             "Continuously (e.g. min-by-min)",
-//             "Periodically (e.g. once an hour)",
-//             "Exactly (quantitatively)",
-//             "Approximately (categorically)"
-//         ]
-//     },
-//     {
-//         numb: 4,
-//         question: "Predict (forecast) a process variable or a key performance indicator",
-//         answer: "",
-//         options: [
-//             "Continuously (e.g. min-by-min)",
-//             "Periodically (e.g. once an hour)",
-//             "Exactly (quantitatively)",
-//             "Approximately (categorically)"
-//         ]
-//     },
-//     {
-//         numb: 5,
-//         question: "Understand causes (influencers) of a process variable or a key performance indicator",
-//         answer: "",
-//         options: [
-//             "Historically (e.g. both 1 week ago and today)",
-//             "In real-time (i.e. over the past 10 min)",
-//             "Of something noted/recorded",
-//             "Of something missed/ not recorded"
-//         ]
-//     },
-//     {
-//         numb: 6,
-//         question: "Find setpoints to improve (optimize) a process variable or a key performance indicator",
-//         answer: "",
-//         options: [
-//             "On average (probabilistically) over a period of time",
-//             "In real time"
-//         ]
-//     },
-//     {
-//         numb: 6,
-//         question: "Find out how operators act in various situations",
-//         answer: "",
-//         options: [
-//             "In response to specific events",
-//             "In general (i.e. infer operating rules)"
-//         ]
-//     },
-// ];
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+];
